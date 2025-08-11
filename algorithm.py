398fdb67842a9fa30ed45c66018693d2f510683e
Aaron Tian <58406472+waiwasabi@users.noreply.github.com>
2025-08-11 01:01:42 -0400

    initial commit

---
+++ algorithm.py
@@ -0,0 +1,46 @@
+import numpy as np
+import unittest
+
+
+def nystrom_approximation(A, S):
+    return A[:, S] @ np.linalg.pinv(A[np.ix_(S, S)]) @ A[S, :]
+
+
+def partial_cholesky(A, k, selector):
+    n = A.shape[0]
+    diag = np.diag(A)
+    F = np.zeros((n, k))
+    S = set()
+    for i in range(k):
+        s = selector(diag)
+        S.add(s)
+        g = A[:, s] - F[:, :i] @ F[s, :i].T
+        F[:, i] = g / np.sqrt(g[s])
+        diag = diag - F[:, i] ** 2
+        diag = diag.clip(min=0)
+        if sum(diag) == 0:
+            break
+    return F, S
+
+
+def adaptive_random(diag):
+    return np.random.choice(len(diag), p=diag/sum(diag))
+
+
+class Test(unittest.TestCase):
+    def test_rpcholesky(self):
+        """
+        Ensure that the Nystrom approximation returned by RPCholesky is correct.
+        """
+        n = 1000
+        A = np.random.random_sample((n, n))
+        A = A @ A.T  # make random psd matrix
+
+        F, S = partial_cholesky(A, 50, adaptive_random)
+        S = list(S)
+        A_hat = nystrom_approximation(A, S)
+        self.assertTrue(np.allclose(A_hat, F @ F.T))
+
+
+if __name__ == "__main__":
+    unittest.main()
